6410f21ec4e799438f012caea629ec3db4a869ff
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-08 23:41:38 +0600

    debug-deposit

--- js/utilities.js
+++ js/utilities.js
@@ -1,19 +1,19 @@
 function getInputFieldValueById(inputFieldId) {
-  const inputField = document.getElementById(inputFieldId);
-  const inputFieldValueString = inputField.value;
-  const inputFieldValue = parseFloat(inputFieldValueString);
-  return inputFieldValue;
-  inputField.value = "";
+	const inputField = document.getElementById(inputFieldId);
+	const inputFieldValueString = inputField.value;
+	const inputFieldValue = parseFloat(inputFieldValueString);
+	return inputFieldValue;
+	inputField.value = "";
 }
 
 function getTextElementValueById(elementId) {
-  const textElement = document.getElementById(elementId);
-  const textElementValueString = textElement.innerText;
-  const textElementValue = textElementValueString;
-  textElementValue;
+	const textElement = document.getElementById(elementId);
+	const textElementValueString = textElement.innerText;
+	const textElementValue = parseFloat(textElementValueString);
+	return textElementValue;
 }
 
 function setTextElementValueById(elementId, newValue) {
-  const textElement = document.getElementById(elementId);
-  textElement.value = newValue;
+	const textElement = document.getElementById(elementId);
+	textElement.innerText = newValue;
 }
