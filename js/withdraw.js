72d9ed72e1df8396d3bf7a8c32527b39d2f8b835
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-08 23:50:22 +0600

    debug-withdraw

--- js/withdraw.js
+++ js/withdraw.js
@@ -10,11 +10,12 @@
 */
 
 document.getElementById("btn-withdraw").addEventListener("click", function () {
-  const newWithdrawAmount = getInputFieldValueById("withdraw-field");
-  const previousWithdrawTotal = getTextElementValueById("withdraw-total ");
-  const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  setTextElementValueById(withdraw - total, newWithdrawTotal);
-  const previousBalanceTotal = getTextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-  setTextElementValueById("balance-total", newBalanceTotal);
+	const newWithdrawAmount = getInputFieldValueById("withdraw-field");
+	const previousWithdrawTotal = getTextElementValueById("withdraw-total");
+	// console.log(previousWithdrawTotal, newWithdrawAmount);
+	const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+	setTextElementValueById("withdraw-total", newWithdrawTotal);
+	const previousBalanceTotal = getTextElementValueById("balance-total");
+	const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+	setTextElementValueById("balance-total", newBalanceTotal);
 });
